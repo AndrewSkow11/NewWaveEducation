09ab160352c399f66029863788fc9cbb5c79a87c
Andrew Skow <skovorodnikov11@gmail.com>
2024-06-11 21:08:45 +0300

    migrate

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Section(models.Model):
+    """Модель описывает разделы учебных матриалов"""
+    title = models.CharField(max_length=255, verbose_name='заголовок')
+    description = models.TextField(verbose_name='описание')
+
+    class Meta:
+        verbose_name = 'раздел'
+        verbose_name_plural = 'разделы'
+
+    def __str__(self):
+        return self.title
+    
