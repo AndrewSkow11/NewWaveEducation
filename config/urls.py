727ad1397157fc7fd0aeba7850da3662b6cb48cb
Andrew Skow <skovorodnikov11@gmail.com>
2024-06-12 21:36:49 +0300

    quiz new version application

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('courses.urls')),
-    path('', include('quiz.urls'))
+    path('quiz/', include('quiz.urls'))
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,23 +1,3 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Quiz, Question
-from .serializers import QuizSerializer, RandomQuestionSerializer, QuizQuestionSerializer
+from django.shortcuts import render
 
-class Quiz(ListAPIView):
-    queryset = Quiz.objects.all()
-    serializer_class = QuizSerializer
-
-
-class RandomQuestion(APIView):
-    def get(self, request, format=None, **kwargs):
-        question = Question.objects.filter(quiz__title=kwargs['topic']).order_by("?")[:1]
-        serializer = RandomQuestionSerializer(question, many=True)
-        return Response(serializer.data)
-
-
-class QuizQuestion(APIView):
-    def get(self, request, format=None, **kwargs):
-        question = Question.objects.filter(quiz__title=kwargs['topic'])
-        serializer = QuizQuestionSerializer(question, many=True)
-        return Response(serializer.data)
+# Create your views here.
