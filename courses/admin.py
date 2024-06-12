83b34bee463e54605b16a21dc6519bfeaf2ebd6e
Andrew Skow <skovorodnikov11@gmail.com>
2024-06-12 11:57:46 +0300

    quiz models serializers.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from quiz.models import (
+    Answer,
+    Category,
+    Question,
+    Quiz
+)
+
 # Register your models here.
+admin.site.register(Answer)
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Quiz)
