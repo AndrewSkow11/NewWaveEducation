2345ab109296adbde28703b4f7dab139de1f3ddb
Andrew Skow <skovorodnikov11@gmail.com>
2024-06-12 23:49:23 +0300

    рабочая модель тестов

--- quiz/models.py
+++ quiz/models.py
@@ -1,19 +1,51 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
 class Quiz(models.Model):
-  name = models.CharField(max_length=300)
+    name = models.CharField(
+        verbose_name='Название теста',
+        max_length=300)
+
+    class Meta:
+        verbose_name = 'тест'
+        verbose_name_plural = 'тесты'
+
+    def __str__(self):
+        return self.name
 
 
 class Question(models.Model):
-  quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-  text = models.CharField(max_length=300)
+    quiz = models.ForeignKey(
+        Quiz,
+        on_delete=models.CASCADE,
+        verbose_name='название теста'
+    )
+    text = models.CharField(
+        max_length=300,
+        verbose_name='текст вопроса'
+    )
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
+
+    def __str__(self):
+        return self.text
 
 
 class Answer(models.Model):
-  question = models.ForeignKey(Question, on_delete=models.CASCADE)
-  text = models.CharField(max_length=300)
-  is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        verbose_name='вопрос')
+    text = models.CharField(
+        max_length=300,
+        verbose_name='текст ответа')
+    is_correct = models.BooleanField(default=False, verbose_name='правильность ответа')
 
+    class Meta:
+        verbose_name = 'ответ'
+        verbose_name_plural = 'ответы'
+
+    def __str__(self):
+        return f'{self.text} {self.is_correct}'
