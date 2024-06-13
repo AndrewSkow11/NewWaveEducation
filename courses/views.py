a3b957c99305d6b1d1cbbf9d5c44477dc13d6357
Andrew Skow <skovorodnikov11@gmail.com>
2024-06-13 17:00:33 +0300

    исправление ошибок авторизации

--- courses/views.py
+++ courses/views.py
@@ -2,27 +2,28 @@ from courses.models import Section, Material
 from rest_framework import generics
 from courses.serializers import MaterialSerializer, SectionSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class MaterialListAPIView(generics.ListAPIView):
+class MaterialListAPIView(LoginRequiredMixin, generics.ListAPIView):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
     permission_classes = [IsAuthenticated]
 
 
-class MaterialRetrieveAPIView(generics.RetrieveAPIView):
+class MaterialRetrieveAPIView(LoginRequiredMixin, generics.RetrieveAPIView):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
     permission_classes = [IsAuthenticated]
 
 
-class SectionListAPIView(generics.ListAPIView):
+class SectionListAPIView(LoginRequiredMixin, generics.ListAPIView):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
     permission_classes = [IsAuthenticated]
 
 
-class SectionRetrieveAPIView(generics.RetrieveAPIView):
+class SectionRetrieveAPIView(LoginRequiredMixin, generics.RetrieveAPIView):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
     permission_classes = [IsAuthenticated]
